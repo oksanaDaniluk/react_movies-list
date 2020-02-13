e759fa226040ed5a567b46e8fa48a213f09f5970
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-02-13 23:00:36 +0200

    Updates

---
+++ precache-manifest.8e64e5113d9c7658f3f6042b45944537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d793e5e3faba3ad34a2be9ea21fef33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee5f99fc7a33a4fd49db",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "00482b569866d4623115",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "ee5f99fc7a33a4fd49db",
+    "url": "./static/js/2.a6f01a67.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.a6f01a67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00482b569866d4623115",
+    "url": "./static/js/main.43c69684.chunk.js"
+  },
+  {
+    "revision": "ef79a84613c281fd2d2e",
+    "url": "./static/js/runtime-main.e88ca7d2.js"
+  }
+]);
